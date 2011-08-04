1782322d75b4b1024db88e4a653af50f7c066f87
Aaron Duffy <aduffy70@gmail.com>
2011-08-03 21:17:27 -0600

    Made the cellular automata settings configurable.

--- CellularAutomataModule.cs
+++ CellularAutomataModule.cs
@@ -44,31 +44,56 @@ namespace CellularAutomataModule
     public class CellularAutomataModule : IRegionModule
     {
         private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-		List<SceneObjectGroup> m_prims = new List<SceneObjectGroup>();
-		int m_xStartPos = 45; //inworld x coordinate for the 0,0 position
-		int m_yStartPos = 120;  //inworld y coordinate for the 0,0 position
-		int m_zStartPos = 21;  //inworld z height for the grid
+		bool m_enabled = false;
+        List<SceneObjectGroup> m_prims = new List<SceneObjectGroup>();
+		float m_xPos = 128f; //inworld x coordinate for the 0,0 position
+		float m_yPos = 128f;  //inworld y coordinate for the 0,0 position
+		float m_zPos = 25f;  //inworld z height for the grid
 		int m_xCells = 16;
 		int m_yCells = 16;
 		float m_offset = 0.25f;
-		float[] m_matrix = new float[16 * 16];
-		//float[] m_newMatrix = new float[16 * 16];
+		float[] m_matrix;
         private Scene m_scene;
         Timer m_timer = new Timer(); //Timer to replace the region heartbeat
         Random m_random = new Random();
+        int m_cycleTime = 10000;
 
         #region IRegionModule interface
 
         public void Initialise(Scene scene, IConfigSource config)
         {
-            m_scene = scene;
+            IConfig cellularAutomataConfig = config.Configs["CellularAutomata"];
+            if (cellularAutomataConfig != null)
+            {
+                m_enabled = cellularAutomataConfig.GetBoolean("enabled", true);
+                m_xPos = cellularAutomataConfig.GetFloat("x_position", 128.0f);
+                m_yPos = cellularAutomataConfig.GetFloat("y_position", 128.0f);
+                m_zPos = cellularAutomataConfig.GetFloat("z_position", 25.0f);
+                m_xCells = cellularAutomataConfig.GetInt("x_cells", 16);
+                m_yCells = cellularAutomataConfig.GetInt("y_cells", 16);
+                m_offset = cellularAutomataConfig.GetFloat("offset", 0.25f);
+                m_cycleTime = cellularAutomataConfig.GetInt("cycle_time", 10) * 1000;
+            }
+            if (m_enabled)
+            {
+                m_matrix = new float[m_xCells * m_yCells];
+                m_scene = scene;
+            }
         }
 
         public void PostInitialise()
         {
-            m_scene.EventManager.OnChatFromWorld += new EventManager.ChatFromWorldEvent(OnChat);
-            m_scene.EventManager.OnChatFromClient += new EventManager.ChatFromClientEvent(OnChat);
-            InitializeMatrix(m_scene);
+            if (m_enabled)
+            {
+                m_scene.EventManager.OnChatFromWorld += new EventManager.ChatFromWorldEvent(OnChat);
+                m_scene.EventManager.OnChatFromClient += new EventManager.ChatFromClientEvent(OnChat);
+                InitializeMatrix(m_scene);
+                //Start the timer
+                m_timer.Elapsed += new ElapsedEventHandler(OnTimer);
+                m_timer.Interval = m_cycleTime;
+                m_timer.Start();
+
+            }
         }
 
         public void Close()
@@ -96,11 +121,11 @@ namespace CellularAutomataModule
         void InitializeMatrix(Scene scene)
         {
             // We're going to place a grid of objects in world
-			for (int x=0; x<m_xCells; x++)
+			for (int y=0; y<m_yCells; y++)
             {
-				for (int y=0; y<m_yCells; y++)
+				for (int x=0; x<m_xCells; x++)
                 {
-					Vector3 pos = new Vector3(m_xStartPos + x, m_yStartPos + y, m_zStartPos);
+					Vector3 pos = new Vector3(m_xPos + x, m_yPos + y, m_zPos);
 					PrimitiveBaseShape prim = PrimitiveBaseShape.CreateBox();
 					prim.Textures = new Primitive.TextureEntry(new UUID("5748decc-f629-461c-9a36-a35a221fe21f"));
 					SceneObjectGroup sog = new SceneObjectGroup(UUID.Zero, pos, prim);
@@ -108,7 +133,7 @@ namespace CellularAutomataModule
 					Primitive.TextureEntry tex = sog.RootPart.Shape.Textures;
 					float randomValue = (float)m_random.NextDouble();
 					Color4 texcolor = new Color4(randomValue, randomValue, randomValue, 1.0f);
-					m_matrix[y * 16 + x] = randomValue;
+					m_matrix[y * m_xCells + x] = randomValue;
 					tex.DefaultTexture.RGBA = texcolor;
 					sog.RootPart.UpdateTexture(tex);
 					m_prims.Add(sog);
@@ -120,10 +145,6 @@ namespace CellularAutomataModule
             {
 				scene.AddNewSceneObject(sogr, false);
             }
-            //Start the timer
-            m_timer.Elapsed += new ElapsedEventHandler(OnTimer);
-            m_timer.Interval = 20000;
-            m_timer.Start();
 		}
 
         void OnChat(Object sender, OSChatMessage chat)
@@ -140,24 +161,28 @@ namespace CellularAutomataModule
                     {
 						m_offset = float.Parse(chat.Message.Substring(6));
 					}
-					for (int x=0; x<m_xCells; x++)
-                    {
-						for (int y=0; y<m_yCells; y++)
-                        {
-                            int index = y * 16 + x;
-							float randomValue = (float)m_random.NextDouble();
-							Color4 texcolor = new Color4(randomValue, randomValue, randomValue, 1.0f);
-							m_matrix[index] = randomValue;
-							Primitive.TextureEntry tex = m_prims[index].RootPart.Shape.Textures;
-							tex.DefaultTexture.RGBA = texcolor;
-                            m_prims[index].RootPart.UpdateTexture(tex);
-                            m_prims[index].ScheduleGroupForTerseUpdate();
-                        }
-                    }
+                    RandomizeMatrix();
                 }
             }
         }
 
+        void RandomizeMatrix()
+        {
+			for (int y=0; y<m_yCells; y++)
+            {
+				for (int x=0; x<m_xCells; x++)
+                {
+                    int index = y * m_xCells + x;
+				    float randomValue = (float)m_random.NextDouble();
+					Color4 texcolor = new Color4(randomValue, randomValue, randomValue, 1.0f);
+					m_matrix[index] = randomValue;
+					Primitive.TextureEntry tex = m_prims[index].RootPart.Shape.Textures;
+					tex.DefaultTexture.RGBA = texcolor;
+                    m_prims[index].RootPart.UpdateTexture(tex);
+                    m_prims[index].ScheduleGroupForTerseUpdate();
+                }
+            }
+        }
 
 		void OnTimer(object source, ElapsedEventArgs e)
         {
@@ -166,57 +191,57 @@ namespace CellularAutomataModule
 
         void UpdateMatrix()
         {
-            float[] newMatrix = new float[16 * 16];
+            float[] newMatrix = new float[m_xCells * m_yCells];
             int rowabove;
             int rowbelow;
             int colleft;
             int colright;
             int xMaxIndex = m_xCells - 1;
             int yMaxIndex = m_yCells - 1;
-            for (int x=0; x<m_xCells; x++)
+            for (int y=0; y<m_yCells; y++)
             {
-				if (x == 0)
+				if (y == 0)
                 {
-					colright = x + 1;
-					colleft = xMaxIndex;
+					rowabove = y + 1;
+					rowbelow = yMaxIndex;
 				}
-				else if (x == xMaxIndex)
+				else if (y == yMaxIndex)
                 {
-					colright = 0;
-					colleft = x - 1;
+					rowabove = 0;
+					rowbelow = y - 1;
 				}
 				else
                 {
-					colright = x + 1;
-					colleft = x - 1;
+					rowabove = y + 1;
+					rowbelow = y - 1;
 				}
-				for (int y=0; y<m_yCells; y++)
+				for (int x=0; x<m_xCells; x++)
                 {
-                    if (y == 0)
+                    if (x == 0)
                     {
-						rowabove = y + 1;
-						rowbelow = yMaxIndex;
+						colright = x + 1;
+						colleft = xMaxIndex;
 					}
-					else if (y == yMaxIndex)
+					else if (x == xMaxIndex)
                     {
-						rowabove = 0;
-						rowbelow = y - 1;
+						colright = 0;
+						colleft = x - 1;
 					}
 					else
                     {
-						rowabove = y + 1;
-						rowbelow = y - 1;
+						colright = x + 1;
+						colleft = x - 1;
 					}
-                    int index = y * 16 + x;
-                    float neighboraverage = ((m_matrix[rowbelow * 16 + colleft] +
-                                              m_matrix[y * 16 + colleft] +
-                                              m_matrix[rowabove * 16 + colleft] +
-                                              m_matrix[rowbelow * 16 + x] +
-                                              m_matrix[rowabove * 16 + x] +
-                                              m_matrix[rowbelow * 16 + colright] +
-                                              m_matrix[y * 16 + colright] +
-                                              m_matrix[rowabove * 16 + colright] +
-                                              m_matrix[index]
+                    int index = y * m_xCells + x;
+                    float neighboraverage = ((m_matrix[rowbelow * m_xCells + colleft] +
+                                              m_matrix[y * m_xCells + colleft] +
+                                              m_matrix[rowabove * m_xCells + colleft] +
+                                              m_matrix[rowbelow * m_xCells + x] +
+                                              m_matrix[index] +
+                                              m_matrix[rowabove * m_xCells + x] +
+                                              m_matrix[rowbelow * m_xCells + colright] +
+                                              m_matrix[y * m_xCells + colright] +
+                                              m_matrix[rowabove * m_xCells + colright]
                                              ) / 9);
 					float newCellValue = (neighboraverage + m_offset) % 1.0f;
                     newMatrix[index] = newCellValue;
@@ -227,7 +252,7 @@ namespace CellularAutomataModule
                     m_prims[index].ScheduleGroupForTerseUpdate();
 				}
 			}
-	        Array.Copy(newMatrix, m_matrix, 16 * 16);
+	        Array.Copy(newMatrix, m_matrix, m_xCells * m_yCells);
         }
     }
 }
